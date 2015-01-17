f72ac8c8154503c7e0cc2a6e1c761e795d3ed508
manavgabhawala <manav1907@gmail.com>
2015-01-16 21:38:07 -0500

    Initial commit from local git

---
+++ 3b1y/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3b1y
+//
+//  Created by Manav Gabhawala on 1/16/15.
+//  Copyright (c) 2015 Manav Gabhawala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
